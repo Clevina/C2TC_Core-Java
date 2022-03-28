640eb6bd6a5870e154ba6b824a8e00438de2bf03
DELL <DELL@DESKTOP-M3LA5J5>
2022-03-28 12:06:42 +0530

    First commit

---
+++ Triangle Pattern/src/Pattern3.java
@@ -0,0 +1,20 @@
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+	  for(int i=0;i<3;i++)
+	  {
+		  for(int k=2;k>=i;k--)
+		  {
+			  System.out.print(" ");		  
+		  }
+	  for( int j=0;j<=i;j++)
+	  {
+		  System.out.print("* ");
+	  }
+	  System.out.println();
+	  }
+
+	}
+
+}
---
+++ Triangle Pattern/src/Pattern4.java
@@ -0,0 +1,25 @@
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=2;i++)
+		  {
+			  for(int k=0;k<i;k++)
+			  {
+				  System.out.print(" ");		  
+			  }
+		  for( int j=0;j<=2-i;j++)
+		  {
+			  System.out.print("*"+" ");
+		  }
+		  System.out.println();
+		  }
+
+		}
+
+	}
+
+
+	
+
+
