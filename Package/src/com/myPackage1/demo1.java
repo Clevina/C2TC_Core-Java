fa878e49d07a6501bb6c7fef98e1b5c2d950848c
DELL <DELL@DESKTOP-M3LA5J5>
2022-04-04 13:16:35 +0530

    First commit

---
+++ Package/src/com/myPackage1/demo1.java
@@ -0,0 +1,14 @@
+package com.myPackage1;
+
+import com.myPackage.demo;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		String name="Welcome to C2TC Programm ";
+		demo d1 =new demo();
+		d1.setname(name);
+		System.out.println(d1.i);
+	}
+
+}
