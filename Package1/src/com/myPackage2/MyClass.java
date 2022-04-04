b8e44732f5ab1a40d6081ddb374024cf2b118168
DELL <DELL@DESKTOP-M3LA5J5>
2022-04-04 13:36:03 +0530

    First commit

---
+++ Package1/src/com/myPackage2/MyClass.java
@@ -0,0 +1,10 @@
+package com.myPackage2;
+
+public class MyClass {
+	public int i=10;
+	public void  getNames(String name) {
+		System.out.println(name);
+		System.out.println(i);
+
+}
+}
---
+++ Package1/src/com/psvm/PrintName.java
@@ -0,0 +1,14 @@
+package com.psvm;
+
+import com.myPackage2.*;
+
+
+public class PrintName {
+
+	public static void main(String[] args) {
+		String name= "Welcome to C2TC Program";
+		MyClass obj= new  MyClass();
+		obj.getNames(name);
+	}
+
+}
