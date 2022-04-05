6aee054fdcd2d392954ef57b3a8e8bd821368a3f
DELL <DELL@DESKTOP-M3LA5J5>
2022-04-05 15:51:36 +0530

    First commit

---
+++ Practice qs/src/Main4.java
@@ -0,0 +1,17 @@
+
+public class Main4 {
+
+	public static void main(String[] args) {
+		boolean b=true;
+		if(b==false)
+		{
+			System.out.println("HELLO");
+		}
+		else {
+			System.out.println("BYE");
+		}
+		
+
+	}
+
+}
---
+++ Practice qs/src/test.java
@@ -0,0 +1,13 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		int i=0;
+		for(System.out.println("HI");i<1;i++)
+		{
+			System.out.println("HELLO PIE");
+		}
+
+	}
+
+}
