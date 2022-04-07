1510df865c7bfc623702ccb56893bd7e143f3b43
DELL <DELL@DESKTOP-M3LA5J5>
2022-04-07 10:37:02 +0530

    first commit

---
+++ Package/src/com/Mypack/ClassB.java
@@ -0,0 +1,18 @@
+package com.Mypack;
+
+import com.pack.ClassA;
+import com.pack.ClassC;
+
+
+public class ClassB {
+
+	public static void main(String[] args) {
+		ClassA obj=new ClassA();
+		ClassC obj1=new ClassC();
+		obj.msg();
+		obj1.msg();
+
+
+	}
+
+}
---
+++ Package/src/com/pack/ClassC.java
@@ -0,0 +1,9 @@
+package com.pack;
+
+public class ClassC {
+
+	public void msg()
+	{
+		System.out.println("Hello fro class C");
+	}
+}
