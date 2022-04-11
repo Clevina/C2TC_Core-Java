a094985bcab04432154ce00d46e8c98376606291
DELL <DELL@DESKTOP-M3LA5J5>
2022-04-11 08:47:55 +0530

    first commit

---
+++ Wrapper Class/src/Demo.java
@@ -0,0 +1,13 @@
+
+public class Demo {//autoboxing primitive to wrapper
+
+	public static void main(String[] args) {
+		int a=10;
+		Integer i=Integer.valueOf(a);
+		Integer j=a;
+		System.out.println(a+" "+i+" "+j);
+		
+
+	}
+
+}
---
+++ Wrapper Class/src/Demo1.java
@@ -0,0 +1,12 @@
+
+public class Demo1 {//wrapper to primitive
+
+	public static void main(String[] args) {
+		Integer a=new Integer(3);
+		int i=a.intValue();//unboxing
+		int  j=a;
+		System.out.println(a+" "+i+""+j);
+
+	}
+
+}
