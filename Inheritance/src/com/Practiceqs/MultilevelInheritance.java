60133ba940e3903e27f72cc15427db06afddcbdf
DELL <DELL@DESKTOP-M3LA5J5>
2022-04-12 13:15:46 +0530

    first commit

---
+++ Inheritance/src/com/Practiceqs/MultilevelInheritance.java
@@ -0,0 +1,44 @@
+package com.Practiceqs;
+
+class Addition{
+	public void add(int a,int b)
+	{
+		System.out.println("Addition is :"+(a+b));
+	}
+}
+class subtraction extends Addition{
+	public void sub(int a,int b)
+	{
+		System.out.println("subtraction is :"+(a-b));
+	}
+}
+class multiplication extends subtraction{
+	public void multiply(int a,int b)
+	{
+		System.out.println("multiplication is is :"+(a*b));
+	}
+}
+class division extends multiplication{
+	public void div(int a,int b)
+	{
+		System.out.println("division is :"+(a/b));
+	}
+	public void CallAllMethods()
+	{
+		add(2,3);
+		sub(4,3);
+		multiply(3,4);
+		div(8,6);
+	}
+}
+class calculation extends division
+{
+public class MultilevelInheritance {
+
+	public static void main(String[] args) {
+		division d= new division();
+		d.CallAllMethods();
+		
+	}
+}
+}
