febb73fc17eb7ba72bf0278ef5c40d0fb07a2eef
DELL <DELL@DESKTOP-M3LA5J5>
2022-05-01 23:29:01 +0530

    first commit

---
+++ Collection/src/arrayList.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class arrayList{
+
+	public static void main(String[] args) {
+		
+		Collection list= new ArrayList();
+		list.add(100);
+		list.add(10);
+		list.add(1000);
+		list.add("Webinar");
+		System.out.println("ArrayList:"+list);
+		
+	}
+
+}
